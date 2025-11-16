89a171c8258d47fbef7f232d4f9c1b88379d7ead
sp8cky <jh4112000@gmx.de>
2025-11-16 14:19:15 +0100

    cleaned ignored files

--- 3n+1/rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\julia\OneDrive\Dokumente\Privat\IT-Projekte\Math-Lab\3n+1\rust\target\debug\deps\rust.d: src\main.rs
-
-C:\Users\julia\OneDrive\Dokumente\Privat\IT-Projekte\Math-Lab\3n+1\rust\target\debug\deps\rust.exe: src\main.rs
-
-src\main.rs:
